71717c94e579ec8d6b02a381999e0a05668cc850
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2025-02-04 20:37:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-set -e 
+# Heroku dynamically assigns a port, use it if needed
+export PORT=${PORT:-5000}
 
+# Ensure all dependencies are installed
+pip3 install --no-cache-dir -r requirements.txt
+
+# Run update script (if needed)
 python3 update.py
-exec python3 -m bot  
 
+# Start the bot
+exec python3 -m bot
