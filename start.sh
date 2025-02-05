d242b539120992d25b192872cf4b3d962e649500
spidy-naik <166608100+spidy-naik@users.noreply.github.com>
2025-02-05 13:35:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Heroku dynamically assigns a port, use it if needed
-export PORT=${PORT:-5000}
+# # Heroku dynamically assigns a port, use it if needed
+# export PORT=${PORT:-5000}
 
-# Ensure all dependencies are installed
-pip3 install --no-cache-dir -r requirements.txt
+# # Ensure all dependencies are installed
+# pip3 install --no-cache-dir -r requirements.txt
 
-# Run update script (if needed)
-python3 update.py
+# # Run update script (if needed)
+# python3 update.py
 
-# Start the bot
-exec python3 -m bot
+# # Start the bot
+# exec python3 -m bot
+python3 update.py && python3 -m bot
